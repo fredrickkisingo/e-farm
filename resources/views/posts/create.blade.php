622f8ec91476c1c2706558055fae41a612b6c064
fred <fredrickkisingo@gmail.com>
2020-03-23 20:12:19 +0300

    removing the ck editor

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -27,7 +27,7 @@
   </div>
   <div class="form-group">
     {{Form::label('body','Product Description')}} 
-    {{Form::textarea('body', '', ['id'=> 'article-ckeditor', 'class'=>'form-control',
+    {{Form::textarea('body', '', [ 'class'=>'form-control',
     'placeholder'=>'Product Description'])}}
   </div>
   <div class="form-group">
