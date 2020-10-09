33b02d16c156cffc3061742b0350f2978bda5895
fred <fredrickkisingo@gmail.com>
2020-10-09 15:34:35 +0300

    minor indentation changes

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,21 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-@include('inc.slider')
-<div class="jumbotron text-center">
-  
-  {{--this is the main index page--}}
-  <h1>{{$title}}</h1>
-<p>
-  EFarm: Your trusted farm products solution
-</p>
-
-
- </div>
-  <footer>
-    <p>Copyright &copy;2020Fredrick Kisingo</p>
-  </footer>
-  @endsection   
+  @include('inc.slider')
+  <div class="jumbotron text-center"> 
+        {{--this is the main index page--}}
+      <h1>{{$title}}</h1>
+      <p>
+        EFarm: Your trusted farm products solution
+      </p>
+  </div>
+    <footer>
+      <p>Copyright &copy;2020 Fredrick Kisingo</p>
+    </footer>
+ @endsection   
 
 {{--this means that the  whole layout(html)will be extended from the layouts.app file and the only changes to be made will be dictated in the respective files of such as index,about and services--}}
 
