3122fbb3a2f04ba27321e749f239b95382314e0d
fred <fredrickkisingo@gmail.com>
2020-11-09 19:40:58 +0300

    adding navigation instructions

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,5 +2,19 @@
 
 @section('content') 
     <h1>About</h1>
-    <p>This is a Web Application that intends to solve the menace of ready markets and also post harvest losses.It is a platform for the farmers and by the farmers!</p>   
+    <p>This is a Web Application that intends to solve the menace of ready markets and also post harvest losses.<b>It is a platform for the farmers and by the farmers!</b></p> 
+    <h2>How to navigate through E-farm</h2>
+    <p>1.For a new user you are to register for a new account as a farmer or buyer by  entering the valida details<br>
+       2.On successful registration as a farmer you are now able to upload new farm produce from the dashboard by clicking on "Add Product" button<br>
+       3.A farmer is to post the farm produce which he or she has in order for them to be seen by the buyers and be purchased<br>
+       4.A buyer on successful login is to browse for farm produce by clicking on the "products" button located on the dashboard then to select from a farmer of his or her choice on the prefference of location,quantity and quality<br>
+       5.As a buyer and a farmer  you are also to read on post harvest losses mitigation ways successfully by clicking on the "Losses mitigation" button<br>
+       6.A buyer is to check on the location of the farmer then to use the "Location pin" to locate the nearest farmer then to input his current location at the "start location" form and that of the farmer at the "end location" form  <br>
+       7.A farmer is to check on his/her farm produce by clicking on his respective name and there will be a list containing the dashboard,add product or logout.You are to click on dashboard to see the products you have uploaded<br>
+       8.A buyer upon interest of a particular farm produce,click on the More Information button of that product then to click "Purchase Product"<br>
+       9.Same action for a farmer who wants to replenish their farm produce in the case they are depleted<br>
+       10.Item will be added to cart then now the user is to  click on "Cart items" located on the top right part of the navigation bar in order to complete payment via Mpesa<br>
+       11.As a registered user you are to input your safaricom Phone number in the (254..)format then click on confirm order as your smartphone phone is open for mpesa to send you a prompt where you are required to input your mpesa pin in order for transaction to be completed<br>
+    </p>
+       
 @endsection
