694a51dc2faba1af208b851897ab864c97f35003
fred <fredrickkisingo@gmail.com>
2020-11-23 17:41:05 +0300

    subsequent releases

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->bigIncrements('role_id');
+            // $table->bigIncrements('role_id');
         });
     }
 
