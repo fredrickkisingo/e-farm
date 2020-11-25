13cb97769207e6bf518cf75873ffddf261eebd88
fred <fredrickkisingo@gmail.com>
2020-11-25 17:36:45 +0300

    adding href

--- resources/views/inc/slider.blade.php
+++ resources/views/inc/slider.blade.php
@@ -1,13 +1,13 @@
 <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active" data-interval="10000">
-            <img src="/storage/sliders/farmer-rice-field-with-laptop_1150-6064.jpg" class="d-block w-100" alt="Slider 1">
+            <img src="http://morning-brook-99254.herokuapp.com/storage/sliders/farmer-rice-field-with-laptop_1150-6064.jpg" class="d-block w-100" alt="Slider 1">
         </div>
         <div class="carousel-item" data-interval="2000">
-            <img src="/storage/sliders/scott-warman-NpNvI4ilT4A-unsplash.jpg" class="d-block w-100" alt="Slider 2">
+            <img src="http://morning-brook-99254.herokuapp.com/storage/sliders/scott-warman-NpNvI4ilT4A-unsplash.jpg" class="d-block w-100" alt="Slider 2">
         </div>
         <div class="carousel-item">
-            <img src="/storage/sliders/smilingfarm2.jpg" class="d-block w-100" alt="Slider 3">
+            <img src="http://morning-brook-99254.herokuapp.com/storage/sliders/smilingfarm2.jpg" class="d-block w-100" alt="Slider 3">
         </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
