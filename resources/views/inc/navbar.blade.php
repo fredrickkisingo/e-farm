a820136bc1ae399c8fa4a32b3b62995241a4fd45
fred <fredrickkisingo@gmail.com>
2021-02-28 15:41:16 +0300

    removsl of spaces

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -51,8 +51,7 @@
                       <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                           {{ Auth::user()->name }} <span class="caret"></span>
                       </a>
-                      
-
+        
                       <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                             <a href="/dashboard" class="dropdown-item">Dashboard</a>
                             @if(Auth::user()->role_id==3)
